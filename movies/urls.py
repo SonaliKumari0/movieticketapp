ba59738c19f0ed21ba052e60c41f6678b5ba0ecc
Sonali Kumari <82748870+SonaliKumari0@users.noreply.github.com>
2025-05-29 15:35:54 +0530

    Update urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('allmovies/', views.all_movies, name='all_movies'),
+    # path('movies/',views.allmovies, name='allmovies'),
+    path('movie/<slug>/', views.movieview, name='movieview'),
+    path('allmovies/', views.filtered_movies, name='filtered_movies'),
 ]
